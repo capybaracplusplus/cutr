2b3f25dd5b3cb4c5c7f61be14eeca559c427326a
Capy <dimak12k@yandex.ru>
2025-11-08 17:26:33 +0300

    implement link service methods

--- src/services/link_service.cpp
+++ src/services/link_service.cpp
@@ -1,4 +1,5 @@
 #include <services/link_service.h>
+#include <utils/shortener.h>
 
 namespace cutr::service {
     LinkService::LinkService(std::shared_ptr<cutr::ILinkRepository> postgresRepo,
@@ -6,13 +7,36 @@ namespace cutr::service {
             : postgresRepo_(std::move(postgresRepo)), redisRepo_(std::move(redisRepo)) {}
 
     drogon::Task<std::string> LinkService::createShortLink(const std::string &originalUrl) {
-        // TODO: Implement this method
-        co_return std::string{};
+        std::string shortCode;
+        bool exists = true;
+
+        while (exists) {
+            shortCode = utils::Shortener::generate(originalUrl);
+
+            auto cached = co_await redisRepo_->getOriginalUrl(shortCode);
+            if (cached.has_value()) {
+                continue;
+            }
+
+            exists = co_await postgresRepo_->exists(shortCode);
+        }
+
+        co_await postgresRepo_->saveLink(shortCode, originalUrl);
+        co_await redisRepo_->saveLink(shortCode, originalUrl);
+
+        co_return shortCode;
     }
 
     drogon::Task<bool> LinkService::shortLinkExists(const std::string &originalUrl) {
-        // TODO: Implement this method
-        co_return false;
+        std::string shortCode = utils::Shortener::generate(originalUrl);
+
+        auto cached = co_await redisRepo_->getOriginalUrl(shortCode);
+        if (cached.has_value()) {
+            co_return true;
+        }
+
+        auto exists = co_await postgresRepo_->exists(shortCode);
+        co_return exists;
     }
 
 } // namespace cutr::service
